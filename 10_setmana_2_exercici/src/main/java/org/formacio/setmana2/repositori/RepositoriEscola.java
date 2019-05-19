074bd09ddc9d70bc8ae7fbb8bdb1d0a20f6209f6
Marcos Monjon Mas <sysmarc13@hotmail.com>
2019-05-19 09:48:25 +0200

    septimo caso test pasado

--- 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
+++ 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
@@ -1,17 +1,27 @@
 package org.formacio.setmana2.repositori;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.formacio.setmana2.domini.Curs;
 import org.formacio.setmana2.domini.Matricula;
+import org.springframework.stereotype.Repository;
 
 /**
  * Modifica aquesta classe per tal que sigui un component Spring que realitza les 
  * operacions de persistencia tal com indiquen les firmes dels metodes
  */
+@Repository
 public class RepositoriEscola {
 
+	@PersistenceContext
+	private EntityManager entityManager = null;
 	
 	public Curs carregaCurs(String nom) {
-		return null;
+		
+		Curs curs = entityManager.find(Curs.class, nom);
+		
+		return curs;
 	}
 	
 	
